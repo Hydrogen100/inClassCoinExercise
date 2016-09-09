6c3c6332c140a0d5c0c0ebbec32c2ad95371d127
Hydrogen100 <180325@hkis.edu.hk>
2016-09-09 18:20:06 +0800

    inClassCoinExerciseFinal

--- src/Coin.java
+++ src/Coin.java
@@ -1,8 +1,10 @@
 import java.awt.Image;
 import java.awt.Graphics;
+
 public class Coin {
 private Image head;
 private Image tail;
+private int currentFace=0; //0 is heads, 1 is tails 
 
 	public Coin(Image a, Image b){
 		head=a;
@@ -10,10 +12,19 @@ private Image tail;
 	}
 	
 	public void flip(){
-		
+		if(currentFace==0){
+			currentFace=1;
+		}else{
+			currentFace=0;
+		}
 	}
 	
 	public void draw(Graphics g, int x , int y){
+		if(currentFace==0){
+			g.drawImage(head, x, y, null);
+		}else{
+			g.drawImage(tail, x, y, null);
+		}
 		
 	}
 	
