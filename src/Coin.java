f7807cab99ac2e1777e9e106af06b54c22a2bf18
Hydrogen100 <180325@hkis.edu.hk>
2016-09-09 12:12:44 +0800

    inClassCoinTest

---
+++ src/Coin.java
@@ -0,0 +1,20 @@
+import java.awt.Image;
+import java.awt.Graphics;
+public class Coin {
+private Image head;
+private Image tail;
+
+	public Coin(Image a, Image b){
+		head=a;
+		tail=b;
+	}
+	
+	public void flip(){
+		
+	}
+	
+	public void draw(Graphics g, int x , int y){
+		
+	}
+	
+}
